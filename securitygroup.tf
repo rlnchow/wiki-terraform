69a9c354352de3b12bc2d829725ecd8c346788f7
Lakshmi <ln.ravipati@yahoo.com>
2018-05-24 16:52:21 +0000

    Secruity group Feature

---
+++ securitygroup.tf
@@ -0,0 +1,37 @@
+data "aws_ip_ranges" "west_east_ec2" {
+  regions       = [ "us-west-2", "us-east-1" ]
+  services      = [ "ec2" ]
+}
+
+resource "aws_security_group" "from_west_east" {
+  name          = "west_east"
+  description     = "Allow only west and east AWS traffic" 
+
+  ingress {
+    from_port   = "443"
+    to_port     = "443"
+    protocol    = "tcp"
+    cidr_blocks = [ "${data.aws_ip_ranges.west_east_ec2.cidr_blocks}"]
+  }
+  tags {
+    Name        = "Allow_east_west"
+    CreateDate  = "${data.aws_ip_ranges.west_east_ec2.create_date}"
+    SyncToken   = "${data.aws_ip_ranges.west_east_ec2.sync_token}"
+  }
+}
+
+resource "aws_security_group" "allow_all" {
+  name          = "allow_all"
+  description   = "Allow all inbound traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name        = "allow_all"
+  }
+}
--- vars.tf
+++ vars.tf
@@ -1,6 +1,6 @@
 # Information about AWS Configs
-variable "AWS_ACCESS_KEY" {}
-variable "AWS_SECRET_KEY" {}
+# variable "AWS_ACCESS_KEY" {}
+# variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
   default = "us-west-2"
 }
